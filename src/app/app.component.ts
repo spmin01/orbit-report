a9d645a87773a5cc2cdb84b43afedac03da9220b
Shaun Minnihan <spminnihan@gmail.com>
2019-10-29 13:34:16 -0500

    Finished main assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,36 @@ import { SourceListMap } from 'source-list-map';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
-    window.fetch(satellitesUrl).then(function(response) {
-      response.json().then(function(data) {
-        for(let i = 0; i < data.satellites.length; i++) {
+    window.fetch(satellitesUrl).then(function (response) {
+      response.json().then(function (data) {
+        for (let i = 0; i < data.satellites.length; i++) {
           let newSatellite = new Satellite(data.satellites[i].name, data.satellites[i].type, data.satellites[i].launchDate, data.satellites[i].orbitType, data.satellites[i].operational);
           this.sourceList.push(newSatellite);
+
+          this.displayList = this.sourceList.slice(0);
         }
       }.bind(this));
     }.bind(this));
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+
+    for(let i = 0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if(name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+
+      this.displayList = matchingSatellites;
+    }
   }
 }
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,11 +5,19 @@ export class Satellite {
     operational: boolean;
     launchDate: string;
 
-    constructor(name: string,  type: string, launchDate: string, orbitType: string, operational: boolean) {
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.orbitType = orbitType;
         this.type = type;
         this.operational = operational;
         this.launchDate = launchDate;
+    }
+
+    shouldShowWarning(): boolean {
+        if (this.type === 'Space Debris') {
+            return true;
+        }
+
+        return false;
     }
 }
